3e1f73842c6d0aad43b4395129d9e5a9df6047fa
atik94 <atikurrahmanatik94@gmail.com>
2022-10-26 14:08:52 +0600

    course title show on left side

--- src/Pages/Shared/LeftSide/LeftSide.js
+++ src/Pages/Shared/LeftSide/LeftSide.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSide = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/courses")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
   return (
     <div>
-      <h2>this is left side</h2>
+      <h2>this is left side {courses.length}</h2>
+      <div>
+        {courses.map((course) => (
+          <p key={course.id}>
+            <Link to="courses">{course.title}</Link>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
